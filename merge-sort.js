362fc88e06bbeffa0938e98f94685b742e37e8b4
Luoxi Pan <l.panpax@gmail.com>
2022-10-22 00:08:36 +1100

    first commit

---
+++ merge-sort.js
@@ -0,0 +1,39 @@
+function merge(A, B) {
+    let i = 0;
+    let j = 0;
+    let result = [];
+
+    while (i < A.length && j < B.length) {
+        if (A[i] < B[j]) {
+            result.push(A[i++]);
+        } else {
+            result.push(B[j++]);
+        }
+    }
+
+    // copy remaining elements in A
+    for (; i < A.length; i++) {
+        result.push(A[i]);
+    }
+    // copy remaining elements in B
+    for (; j < B.length; j++) {
+        result.push(B[j]);
+    }
+    return result;
+}
+
+function merge_sort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
+
+    let leftArr = merge_sort(arr.slice(0, arr.length / 2));
+    let rightArr = merge_sort(arr.slice(arr.length / 2, arr.length));
+
+    return merge(leftArr, rightArr);
+}
+
+let unsortedA = [5, 2, 1, 3, 6, 4];
+let unsortedB = [9, 3, 7, 5, 6, 4, 8, 2];
+
+console.log(merge_sort(unsortedB));
